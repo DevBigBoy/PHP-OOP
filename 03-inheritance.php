002f5b207ac7d007ad1c2f97356a93d8d0e76005
Mohamed Elshazly <56097354+DevBigBoy@users.noreply.github.com>
2023-01-04 05:45:25 +0200

    Add inheritance

--- 03-inheritance.php
+++ 03-inheritance.php
@@ -1,114 +1,103 @@
 <?php
 
-   /**
-     * ===================================
-     *  OOP - object oriented programming
-     * ===================================
-     * *********** DAY03 *****************
-     * ===================================
-     * Inheritance
-   */
-    
-   // Class
-   class Student{
-      protected $name;
-      protected $age;
-      protected $phone;
-      protected $score;
-      protected $subjects = array(
-         'arabic'  => 0,
-         'english' => 0,
-         'math'    => 0,
-         'science' => 0
-      );
-      protected $minScore = 100;
-      protected $maxScore = 200;
+/**
+ * 
+ * OOP - Object Oriented Programing
+ * 
+ * inheritance
+ * 
+ */
 
-      // constructor
-      public function __construct($name, $age){
-         $this->setName($name);
-         $this->setAge($age);
-      }
+class Student
+{
+  private $name;
+  private $age;
+  private $phone;
+  private $score;
+  private $subjects = array(
+    'arabic'  => 0,
+    'english' => 0,
+    'math'    => 0,
+    'science' => 0
+  );
+  private $minScore = 100;
+  private $maxScore = 200;
 
-      private function setName($name){
-        $name = strtolower($name);
-        $name = ucwords($name);
-        $this->name = $name;
-      }
-    
-      public function getName(){
-        return $this->name;
-      }
-      private function setAge($age){
-        $age = filter_var($age, FILTER_SANITIZE_NUMBER_INT);
-        $age = abs($age);
-        if ($age < 20 || $age > 30) {
-            throw new Exception('Sorry age must be between 20 and 30');
-        } else{
-            $this->age = $age;
-        }
-      }
-      public function getAge(){
-        return $this->age;
-      }
-      
-      public function getSubjects(){
-        return $this->subjects;
-      }
-    
-      public function getminScore(){
-        return $this->minScore;
-      }
+  // constructor
+  public function __construct($name, $age, $phone)
+  {
+    $this->setName($name);
+    $this->setAge($age);
+    $this->setPhone($phone);
+    // echo 'Iam a Student\'s constructor ';
+    return $this;
+  }
 
-      public function getMaxScore(){
-        return $this->maxScore;
-      }
+  private function setName($name)
+  {
+    $this->name = ucwords(strtolower($name));
+  }
 
-      public function setSubjectScore($subjectName, $value){
-        if(array_key_exists($subjectName, $this->subjects)){
-            $value = filter_var($value, FILTER_SANITIZE_NUMBER_INT);
-            $value =abs($value);
-            if ($value > 50) {
-                throw new Exception('Sorry The maximum score for this subject is 50');
-            } else{
-                $this->subjects[$subjectName] = $value;
-            }
-        } else{
-            throw new Exception("Sorry This {$subjectName} not exist!");
-        }
-      } 
+  public function getName()
+  {
+    return $this->name;
+  }
 
-      public function getSubjectScore($subjectName){
-        if(array_key_exists($subjectName, $this->subjects)){
-            return $this->subjects[$subjectName];
-        } else{
-            throw new Exception("Sorry This {$subjectName} not exist!");
-        }
-      }
+  private function setAge($age)
+  {
+    $this->age = abs($age);
+  }
+
+  public function getAge()
+  {
+    return $this->age;
+  }
+
+  private function setPhone($phone)
+  {
+    $this->phone = $phone;
+  }
+
+  public function getPhone()
+  {
+    return $this->phone;
+  }
 }
 
-// inhertance
-class Grade1Student extends Student{
-    private $activity = '';
-    public function __construct($name, $age){
-        parent::__construct($name, $age);
-        $this->minScore = 125;
-        $this->maxScore = 250;
-        $this->subjects['computer'] = 0;
-    }
+// $s = new Student('ali', 20, '01025369885');
+// echo $s->getName();
+// echo $s->getAge();
+// echo $s->getPhone();
 
-    public function setStudentAsActive(){
-        $this->activity = 'Active';
-    }
-    public function setStudentAsNoneActive(){
-        $this->activity = 'Not Active';
-    }
+
+class Graduate extends Student
+{
+  protected $year;
+  protected $degree;
+
+  public function __construct($year, $degree, $name, $age, $phone)
+  {
+    parent::__construct($name, $age, $phone);
+    // echo 'Iam a Graduate\'s constructor ';
+    $this->minScore = 125;
+    $this->maxScore = 250;
+    $this->subjects['computer'] = 0;
+    $this->year = $year;
+    $this->degree = $degree;
+  }
+
+  public function getYear()
+  {
+    return $this->year;
+  }
+
+  public function getDegree()
+  {
+    return $this->degree;
+  }
 }
 
-$ali = new Grade1Student('ali gad', 21);
-$ali->setSubjectScore('arabic', 25);
-echo $ali->getSubjectScore('arabic');
-// echo"<pre>";
-// print_r($ali->getSubjects());
-// echo "</pre>";
-?>
+$g = new Graduate('2024', 'good', 'ali', 20, '01025369885');
+echo $g->getName();
+
+// var_dump($g);
