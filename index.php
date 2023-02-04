5a8a506f987082ec3ba4246773ed086330f40f67
Mohamed Elshazly <56097354+DevBigBoy@users.noreply.github.com>
2023-02-05 01:50:00 +0200

    add Day 07

--- index.php
+++ index.php
@@ -1,93 +1,108 @@
 <?php
 
-  /**
-    * ===================================
-    *  OOP - object oriented programming
-    * ===================================
-    * *********** DAY 06 ****************
-    * ===================================
-    * Abstract classes and interfaces
-    * 
-  */
+/**
+ * ===============================
+ *    Magic Methods - Part 1
+ * ===============================
+ * 
+ ** __set
+ * TODO:: runs when writing data to inaccessible property
+ ** __get
+ * TODO:: runs when a call is made to inaccessible property
+ ** __isset
+ * TODO:: triggered when calling isset or empty on inaccessible property
+ ** __unset
+ * TODO:: triggered when invoking unset on inaccessible property     
+ ** __call
+ * TODO:: triggered when invoking inaccessible method in an object context   
+ ** __callStatic
+ * TODO:: triggered when invoking inaccessible method in a static context
+*/
 
-  abstract class Employee{
-    public $Fname;
-    public $Lname;
-    public $age;
-    public $phone;
-    public $address;
-    public $salary;
-    public $tax;
-    public $overtime;
-    public $overtimeRate;
-    public $abbsent;
-    public $abbsentRate;
+class Student {
 
 
-    abstract public function showName();
+  private $data = [];
+  private $name;
+  
 
-    public function getOvertime(){
-      return $this->overtime * $this->overtimeRate;
-    }
-    public function getAbbsent(){
-      return $this->abbsent * $this->abbsentRate;
-    }
+  public function __set($name, $value){
+    $this->data[$name] = $value;
+    
+  }
+// this function was called when ask about the age so the $key is $age
+  public function __get($key)
+  {
+    // age
+    // TODO:: calling __isset function and passing to it $age
 
-    public function getSalary(){
-      return $this->salary - ($this->salary  * $this->tax);
-    }
-
-    abstract public function showTotalSalary();
-
+    //if isset($this->$key) => true  
+    if (isset($this->$key)) {
+      return $this->data[$key];
+    } else {
+        //if isset($this->$key) => false
+        trigger_error('sorry no key '.$key.' Found in the data array', E_USER_ERROR);
+      }
   }
 
-  class Manager extends Employee{
-    public $numberOfAudits;
-    public function showName(){
-      return ' Hello Manager ' . $this->Fname .' ' .$this->Lname;
+  public function __isset($name){
+    //TODO:: it will check that $age exist in $data[]
+    if (array_key_exists($name, $this->data)) {
+      echo 'the property '. $name.' is found and is being extracted ';
+      //TODO:: trigger the above if
+      return true;
+    } else{
+      return False;
+      // echo 'the property '. $name.' not found';
     }
 
-    public function showTotalSalary(){
-      return $this->getSalary() + $this->getOvertime()  - $this->getAbbsent()+ $this->numberOfAudits * 100;
-    }
-
-    public function showAddress(){
-      return $this->address;
-    }
+    // if (property_exists($this, $name)) {
+    //   echo 'the property is found';
+    // } else {
+      
+    // }
+    // echo 'the property '.$name.' will be checked';
   }
 
-  class SuperVisor extends Employee{
-    public $successfullprojects;
-    public function showName(){
-      return ' Hello Supervisor ' . $this->Fname .' ' .$this->Lname;
-    }
-    public function showTotalSalary(){
-      return $this->getSalary() + $this->getOvertime()  - $this->getAbbsent()  + $this->successfullprojects * 1000;
-    }
-  }
-
-  class Worker extends Employee{
-    public $bonus = 100;
-    public function showName(){
-      return ' Hello Worker ' . $this->Fname .' ' .$this->Lname;
-    }
-    public function showTotalSalary(){
-      return $this->getSalary() + $this->getOvertime()  - $this->getAbbsent() + $this->bonus;
+  public function __unset($name)
+  {
+    if(isset($this->$name)){
+      unset($this->data[$name]);
     }
   }
 
+  public function __call($name, $arguments)
+  {
+    if (method_exists($this, $name)) {
+      $this->$name($arguments);
+    } else {
+      trigger_error('function '.$name. ' is not found', E_USER_ERROR);
+    }
+  }
+  public static function __callStatic($name, $arguments)
+  {
+    echo 'A call to a static method '.$name .' occured';
+  }
+
+  private function sayWelcome(){
+    echo 'hello user';
+  }
+  
+}
+
+$ali = new Student();
+$ali->name = 'ali';
+$ali->age = 16;
+$ali->city = 'mansoura';
 
 
-// cannot instantiate abstract class 
+// echo $ali->age;
 
-$mohamed  = new Manager();
-$mohamed->tax = 0.1;
-$mohamed->salary = 5000;
-$mohamed->overtime = 15;
-$mohamed->overtimeRate=30;
-$mohamed->abbsent = 3;
-$mohamed->abbsentRate = 45;
-$mohamed->numberOfAudits = 10;
+// unset($ali->age);
+// echo $ali->age;
+// echo $ali->test;
 
-echo $mohamed->showTotalSalary(). ' L.E';
+// $ali->sayWelcome();
+Student::sayHello();
+
 ?>
